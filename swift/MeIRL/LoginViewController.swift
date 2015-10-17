7697767039dbefb803197d0926e5115afaa1e715
Jeffrey Chang <jeffreychang@pal-nat186-130-133.itap.purdue.edu>
2015-10-17 08:50:45 -0500

    Added iOS files

---
+++ swift/MeIRL/LoginViewController.swift
@@ -0,0 +1,54 @@
+//
+//  LoginViewController.swift
+//  MeIRL
+//
+//  Created by Jeffrey Chang on 10/17/15.
+//  Copyright © 2015 Jeffrey Chang. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var username: UITextField!
+    @IBAction func login(sender: AnyObject) {
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func submit(sender: AnyObject) {
+        Alamofire.request(.GET, "http://httpbin.org/get", parameters: ["foo": "bar"])
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                
+                if let JSON = response.result.value {
+                    print("JSON: \(JSON)")
+                }
+        }
+        username.text
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ swift/MeIRL/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MeIRL
+//
+//  Created by Jeffrey Chang on 10/17/15.
+//  Copyright © 2015 Jeffrey Chang. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
