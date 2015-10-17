48b74765848a46d7ca75fad7bbcd88ee3b6319f6
Kyle Mattimore <kmattimo@umich.edu>
2015-10-17 07:08:54 -0400

    added Volkswagon test suite

---
+++ modules/api/tests/api.server.model.tests.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var should = require('should'),
+  mongoose = require('mongoose');
+  
+  
+describe('Article Model Unit Tests:', function () {
+
+  beforeEach(function (done) {
+  
+  });
+  
+  done();
+});
+
+
+
